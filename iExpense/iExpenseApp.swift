e55dd5a2bb823591585032890dad58508e9dcf45
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-21 14:48:49 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
