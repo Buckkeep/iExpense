40b960f6ac11eba31d5f16903d0ec92530a13fbb
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-21 16:33:40 +0100

    Building a list from which items may be deleted.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,17 +7,41 @@
 
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
 
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
 
 struct ContentView: View {
+    
+    @State private var expenses = Expenses()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in 
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Text", type: "Personal", amount: 5.0)
+                    expenses.items.append(expense)
+                }
+            }
         }
-        .padding()
+    }
+    
+    func removeItems(at offsets: IndexSet ) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
